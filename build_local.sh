7d4ca60d77a77c50152de85317731447ca43b832
Henry Scheible <henry.scheible@gmail.com>
2023-06-03 14:23:44 -0400

    change build local script

--- build_local.sh
+++ build_local.sh
@@ -1,6 +1,6 @@
-docker build ./deps_image -t plmbias-deps
-docker build . -t plmbias
-docker build ./experiments/train -t train
-docker build ./experiments/shapley -t shapley
-docker build ./experiments/ablation -t ablation
-docker build ./experiments/ss_ablation -t ss_ablation
+docker build ./deps_image -t henryscheible/plmbias-deps:latest
+docker build . -t henryscheible/plmbias:latest
+docker build ./experiments/train -t henryscheible/train:latest
+docker build ./experiments/shapley -t henryscheible/shapley:latest
+docker build ./experiments/ablation -t henryscheible/ablation:latest
+docker build ./experiments/ss_ablation -t henryscheible/ss_ablation:latest
