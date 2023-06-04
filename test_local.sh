7d815b8e14bc03192eefa0c529742b1b21ad77fe
Henry Scheible <henry.scheible@gmail.com>
2023-06-03 21:37:07 -0400

    update ci and dockerfile

--- test_local.sh
+++ test_local.sh
@@ -1,10 +1,10 @@
 docker build ./deps_image -t henryscheible/plmbias-deps:latest && \
 docker build . -t henryscheible/plmbias:latest && \
-# docker build ./experiments/train -t henryscheible/train:latest && \
+docker build ./experiments/train -t henryscheible/train:latest && \
 docker build ./experiments/shapley -t henryscheible/shapley:latest && \
-# docker build ./experiments/ablation -t henryscheible/ablation:latest && \
-# docker build ./experiments/ss_ablation -t henryscheible/ss_ablation:latest && \
-# docker run -e IS_TEST=true -e WANDB_API_KEY=$WANDB_TOKEN --gpus all -e CUDA_VISIBLE_DEVICES=0 --rm henryscheible/train:latest && \
+docker build ./experiments/ablation -t henryscheible/ablation:latest && \
+docker build ./experiments/ss_ablation -t henryscheible/ss_ablation:latest && \
+docker run -e IS_TEST=true -e WANDB_API_KEY=$WANDB_TOKEN --gpus all -e CUDA_VISIBLE_DEVICES=0 --rm henryscheible/train:latest && \
 docker run -e IS_TEST=true -e WANDB_API_KEY=$WANDB_TOKEN --gpus all -e CUDA_VISIBLE_DEVICES=0 --rm henryscheible/shapley:latest && \
 docker run -e IS_TEST=true -e WANDB_API_KEY=$WANDB_TOKEN --gpus all -e CUDA_VISIBLE_DEVICES=0 --rm henryscheible/ablation:latest
 
