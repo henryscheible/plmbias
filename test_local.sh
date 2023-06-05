c94c659663a1076cda22059283a0656872c9ae7a
Henry Scheible <henry.scheible@gmail.com>
2023-06-05 10:36:13 -0400

    update local testing schemes

--- test_local.sh
+++ test_local.sh
@@ -1,3 +1,4 @@
-docker build . -t henryscheible/plmbias:latest && \
+docker build . -t henryscheible/plmbias:latest -t ghcr.io/henryscheible/plmbias:latest && \
 docker build ./experiments/train -t henryscheible/train:latest && \
-docker run -e IS_TEST=true -e WANDB_API_KEY=$WANDB_TOKEN --gpus all -e CUDA_VISIBLE_DEVICES=0 --rm henryscheible/train:latest
+docker run -e IS_TEST=true -e WANDB_API_KEY=$WANDB_TOKEN --gpus all -e CUDA_VISIBLE_DEVICES=0 --rm henryscheible/train:latest 
+docker rmi ghcr.io/henryscheible/plmbias:latest
